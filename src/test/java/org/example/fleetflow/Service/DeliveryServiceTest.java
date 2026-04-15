08c8c89047c02f9eaaa67cf90ca8481041588d4e
Reda-benbaih <wareda145@gmail.com>
2026-04-15 10:38:12 +0200

    need 1 more test

---
+++ src/test/java/org/example/fleetflow/Service/DeliveryServiceTest.java
@@ -0,0 +1,124 @@
+package org.example.fleetflow.Service;
+
+import org.example.fleetflow.DTO.delivery.DeliveryGetDTO;
+import org.example.fleetflow.DTO.delivery.DeliveryPostDTO;
+import org.example.fleetflow.entities.*;
+import org.example.fleetflow.mapper.DeliveryMapper;
+import org.example.fleetflow.repositories.ClientRepository;
+import org.example.fleetflow.repositories.DeliveryRepository;
+import org.example.fleetflow.repositories.DriverRepository;
+import org.example.fleetflow.repositories.VehicleRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class DeliveryServiceTest {
+
+    @Mock
+    private DeliveryRepository deliveryRepository;
+    @Mock
+    private DeliveryMapper deliveryMapper;
+    @Mock
+    private ClientRepository clientRepository;
+    @Mock
+    private DriverRepository driverRepository;
+    @Mock
+    private VehicleRepository vehicleRepository;
+    @Mock
+    private DeliveryGetDTO deliveryGetDTO;
+    @InjectMocks
+    private DeliveryService deliveryService;
+
+    DeliveryPostDTO deliveryPostDTO;
+    Delivery delivery;
+    Client client;
+    Driver driver;
+    Vehicle vehicle;
+
+    @BeforeEach
+    void setup(){
+        deliveryPostDTO = DeliveryPostDTO.builder()
+                .deliveryDate(LocalDate.now())
+                .startAddress("start")
+                .endAddress("end")
+                .deliveryStatus(DeliveryStatus.IN_PROGRESS)
+                .clientId(1)
+                .driverId(1)
+                .vehicleId(1)
+                .build();
+
+        deliveryGetDTO = DeliveryGetDTO.builder()
+                .deliveryDate(LocalDate.now())
+                .startAddress("start")
+                .endAddress("end")
+                .deliveryStatus(DeliveryStatus.IN_PROGRESS)
+                .build();
+
+        delivery = Delivery.builder()
+                .deliveryDate(LocalDate.now())
+                .startAddress("start")
+                .endAddress("end")
+                .deliveryStatus(DeliveryStatus.IN_PROGRESS)
+                .build();
+
+        client = Client.builder()
+                .id(1)
+                .name("reda")
+                .email("ertyu@wertyui")
+                .city("WERTYUIO")
+                .number(1234678L)
+                .build();
+
+        driver = Driver.builder()
+                .id(1)
+                .name("Reda")
+                .number(1234567L)
+                .licenceType("b")
+                .available(true)
+                .build();
+
+        vehicle = Vehicle.builder()
+                .id(1)
+                .registrationNumber("qwertyui")
+                .type("wertyuio")
+                .capacity(123456)
+                .vehicleStatus(VehicleStatus.DELIVERY)
+                .build();
+    }
+    @Test
+    void ajouterDeliveryTest() {
+
+        when(deliveryMapper.toEntity(deliveryPostDTO)).thenReturn(delivery);
+        when(clientRepository.findById(client.getId())).thenReturn(Optional.ofNullable(client));
+        when(driverRepository.findById(driver.getId())).thenReturn(Optional.ofNullable(driver));
+        when(vehicleRepository.findById(vehicle.getId())).thenReturn(Optional.ofNullable(vehicle));
+
+        delivery.setClient(client);
+        delivery.setDriver(driver);
+        delivery.setVehicle(vehicle);
+
+        when(deliveryRepository.save(delivery)).thenReturn(delivery);
+        when(deliveryMapper.toGetDTO(delivery)).thenReturn(deliveryGetDTO);
+
+        DeliveryGetDTO result = deliveryService.ajouterDelivery(deliveryPostDTO);
+
+        assertEquals(deliveryGetDTO,result);
+    }
+
+    @Test
+    void updateDeliveryStatusTest(){
+
+    }
+
+
+}
---
+++ src/test/java/org/example/fleetflow/Service/DriverServiceTest.java
@@ -0,0 +1,70 @@
+package org.example.fleetflow.Service;
+
+import lombok.AllArgsConstructor;
+import org.example.fleetflow.DTO.driver.DriverGetDTO;
+import org.example.fleetflow.entities.Driver;
+import org.example.fleetflow.mapper.DriverMapper;
+import org.example.fleetflow.repositories.DriverRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class DriverServiceTest {
+    List<Driver> mockDriverList;
+    List<DriverGetDTO> mockDriverListDTO;
+    @BeforeEach
+    void setup(){
+        Driver driver = Driver.builder()
+                .id(1)
+                .name("Reda")
+                .number(1234567L)
+                .licenceType("b")
+                .available(true)
+                .build();
+
+        DriverGetDTO driverGetDTO = DriverGetDTO.builder()
+                .id(1)
+                .name("Reda")
+                .number(1234567L)
+                .available(true)
+                .build();
+
+        mockDriverList = List.of(driver);
+        mockDriverListDTO = List.of(driverGetDTO);
+
+    }
+
+    @Mock
+    private DriverRepository driverRepository;
+
+    @Mock
+    private DriverMapper driverMapper;
+
+    @InjectMocks
+    private DriverService driverService;
+
+    @Test
+    @DisplayName("first test")
+    void shouldReturnAvailableDrivers() {
+        when(driverRepository.findByAvailableTrue()).thenReturn(mockDriverList);
+
+        when(driverMapper.toGetDTO(mockDriverList.get(0))).thenReturn(mockDriverListDTO.get(0));
+
+        List<DriverGetDTO> result = driverService.getAllDriversAvailable();
+
+        assertEquals(1, result.size());
+        assertEquals(mockDriverListDTO,result);
+        verify(driverRepository).findByAvailableTrue();
+    }
+}
